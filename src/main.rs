c2b61876a2471f1ba60e7201791f0b22166a8a61
Elmar Athmer <elmar@athmer.org>
2025-08-29 18:10:11 +0200

    add tracing to the operator

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use k8s_openapi::api::core::v1::ConfigMap;
 use kube::{Api, Client, Error, api::ListParams};
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
     let client = Client::try_default().await?;
 
     let cms = Api::<ConfigMap>::default_namespaced(client);
 
     let params = ListParams::default().labels("tag=elmar");
+    // let params = ListParams::default();
 
     let cms = cms.list(&params).await?;
 
-    println!("{:#?}", cms);
+    info!("{:#?}", cms);
 
     Ok(())
 }
