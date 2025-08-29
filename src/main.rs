6f181778546c844a617773d7718b3555b20985f1
Elmar Athmer <elmar@athmer.org>
2025-08-29 16:37:29 +0200

    create initial API client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use k8s_openapi::api::core::v1::ConfigMap;
+use kube::{Api, Client, Error, api::ListParams};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let client = Client::try_default().await?;
+
+    let cms = Api::<ConfigMap>::default_namespaced(client);
+
+    let params = ListParams::default().labels("tag=elmar");
+
+    let cms = cms.list(&params).await?;
+
+    println!("{:#?}", cms);
+
+    Ok(())
 }
