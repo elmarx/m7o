81226a96c6761c88d2c436216f648e1792d045cf
Elmar Athmer <elmar@athmer.org>
2026-02-01 12:12:20 +0100

    implement service type

--- src/v1.rs
+++ src/v1.rs
@@ -1,6 +1,7 @@
 use kube::CustomResource;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
 
 #[derive(CustomResource, Deserialize, Serialize, Clone, Debug, JsonSchema)]
 #[kube(
@@ -10,5 +11,22 @@ use serde::{Deserialize, Serialize};
     namespaced
 )]
 pub struct BrokerSpec {
-    desc: String,
+    pub desc: String,
+    #[serde(default)]
+    pub service: ServiceConfig,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, JsonSchema, Default)]
+pub struct ServiceConfig {
+    #[serde(rename = "type", default)]
+    pub type_: ServiceType,
+    #[serde(default)]
+    pub annotations: BTreeMap<String, String>,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, JsonSchema, Default, strum::Display)]
+pub enum ServiceType {
+    #[default]
+    ClusterIP,
+    LoadBalancer,
 }
