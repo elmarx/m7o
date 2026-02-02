ea62c2fa2565efd23cd2c5168debc60724863028
Elmar Athmer <elmar@athmer.org>
2026-02-02 12:12:59 +0100

    mount a mosquitto.conf into the deployment

--- src/deployment.rs
+++ src/deployment.rs
@@ -1,20 +1,21 @@
 use crate::v1::MqttBroker;
 use crate::{MOSQUITTO_VERSION, labels};
 use k8s_openapi::api::apps::v1::{Deployment, DeploymentSpec};
-use k8s_openapi::api::core::v1::{Container, ContainerPort, PodSpec, PodTemplateSpec};
+use k8s_openapi::api::core::v1::{ConfigMap, Container, ContainerPort, PodSpec, PodTemplateSpec};
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::{LabelSelector, ObjectMeta};
 use kube::Resource;
 
 impl MqttBroker {
-    #[must_use] 
+    #[must_use]
     pub fn deployment(&self) -> Deployment {
         let oref = self.controller_owner_ref(&()).unwrap();
-        let labels = labels::metadata(self.metadata.name.as_ref().unwrap(), MOSQUITTO_VERSION);
-        let selector = labels::selector(self.metadata.name.as_ref().unwrap());
+        let name = self.metadata.name.as_ref().unwrap();
+        let labels = labels::metadata(name, MOSQUITTO_VERSION);
+        let selector = labels::selector(name);
 
         Deployment {
             metadata: ObjectMeta {
-                name: self.metadata.name.clone(),
+                name: name.to_string().into(),
                 namespace: self.metadata.namespace.clone(),
                 owner_references: Some(vec![oref]),
                 labels: Some(labels.clone()),
@@ -39,8 +40,21 @@ impl MqttBroker {
                                 name: Some("mqtt".to_string()),
                                 ..ContainerPort::default()
                             }]),
+                            volume_mounts: Some(vec![k8s_openapi::api::core::v1::VolumeMount {
+                                name: "m7o-cfg".to_string(),
+                                mount_path: "/mosquitto/config".to_string(),
+                                ..Default::default()
+                            }]),
                             ..Container::default()
                         }],
+                        volumes: Some(vec![k8s_openapi::api::core::v1::Volume {
+                            name: "m7o-cfg".to_string(),
+                            config_map: Some(k8s_openapi::api::core::v1::ConfigMapVolumeSource {
+                                name: name.clone(),
+                                ..Default::default()
+                            }),
+                            ..Default::default()
+                        }]),
                         ..PodSpec::default()
                     }),
                 },
