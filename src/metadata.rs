9c39c72942dc3aea810149cd6c0941deda38adaf
Elmar Athmer <elmar@athmer.org>
2026-02-08 18:06:36 +0100

    feat: implement instantiation of deployment and service

---
+++ src/metadata.rs
@@ -0,0 +1,11 @@
+use crate::v1::MqttBroker;
+
+impl MqttBroker {
+    #[must_use]
+    pub fn namespace(&self) -> &str {
+        self.metadata
+            .namespace
+            .as_ref()
+            .expect("CRD without namespace")
+    }
+}
