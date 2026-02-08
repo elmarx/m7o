99a5d235c8d96df7ce2a985f2010f97911919f99
Elmar Athmer <elmar@athmer.org>
2026-02-08 18:06:36 +0100

    create secrets for MqttUsers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::sync::Arc;
 use std::time::Duration;
 
-use crate::v1::MqttBroker;
-use futures::{StreamExt, future};
+use crate::v1::{MqttBroker, MqttUser};
+use futures::{StreamExt, future, stream};
 use k8s_openapi::api::apps::v1::Deployment;
-use k8s_openapi::api::core::v1::{ConfigMap, Service};
-use kube::api::PatchParams;
+use k8s_openapi::api::core::v1::{ConfigMap, Secret, Service};
+use kube::api::{ListParams, PatchParams, PostParams};
+use kube::runtime::reflector::ObjectRef;
 use kube::runtime::{Controller, watcher};
 use kube::{Api, Client, Error, ResourceExt, runtime::controller::Action};
 use tracing::{error, info};
@@ -13,6 +14,7 @@ use tracing::{error, info};
 mod broker;
 mod cm_ext;
 mod labels;
+mod user;
 mod util;
 pub mod v1;
 
@@ -33,6 +35,7 @@ async fn main() -> Result<(), Error> {
 
     let client = Client::try_default().await?;
     let broker = Api::<MqttBroker>::all(client.clone());
+    let users = Api::<MqttUser>::all(client.clone());
 
     let deployments = Api::<Deployment>::all(client.clone());
     let services = Api::<Service>::all(client.clone());
@@ -42,6 +45,13 @@ async fn main() -> Result<(), Error> {
         .owns(deployments, watcher::Config::default())
         .owns(services, watcher::Config::default())
         .owns(configmaps, watcher::Config::default())
+        .watches(users, watcher::Config::default(), |user| {
+            let broker_name = user.spec.broker_ref.name.clone();
+            Some(
+                ObjectRef::new(&broker_name)
+                    .within(user.namespace().as_deref().expect("user namespace")),
+            )
+        })
         .shutdown_on_signal()
         .run(reconcile, error_policy, Arc::new(Data { client }))
         .for_each(|_| future::ready(()))
@@ -59,17 +69,49 @@ pub const MANAGER: &str = "m7o.athmer.cloud";
 async fn reconcile(obj: Arc<MqttBroker>, ctx: Arc<Data>) -> Result<Action> {
     info!("reconcile request: {}", obj.name_any());
 
+    let deployment_api = Api::<Deployment>::namespaced(ctx.client.clone(), obj.namespace());
+    let service_api = Api::<Service>::namespaced(ctx.client.clone(), obj.namespace());
+    let configmap_api = Api::<ConfigMap>::namespaced(ctx.client.clone(), obj.namespace());
+    let users_api = Api::<MqttUser>::namespaced(ctx.client.clone(), obj.namespace());
+    let secrets_api = Api::<Secret>::namespaced(ctx.client.clone(), obj.namespace());
+
+    let users = match users_api.list(&ListParams::default()).await {
+        Ok(list) => list
+            .items
+            .into_iter()
+            .filter(|u| u.spec.broker_ref.name == obj.name_any())
+            .collect::<Vec<_>>(),
+        Err(e) => {
+            error!("failed to list users: {:#?}", e);
+            vec![]
+        }
+    };
+
+    let secrets_to_create = stream::iter(users)
+        .filter_map(|user| {
+            let api = secrets_api.clone();
+            async move {
+                let secret = user.secret();
+                let name = secret.metadata.name.as_deref()?;
+                match api.get_opt(name).await {
+                    Ok(None) => Some(secret),
+                    _ => None,
+                }
+            }
+        })
+        .collect::<Vec<_>>()
+        .await;
+
+    for secret in secrets_to_create {
+        let secret_result = secrets_api.create(&PostParams::default(), &secret).await;
+        if let Err(e) = secret_result {
+            error!("failed to create secret {:#?}", e);
+        }
+    }
+
     let configmap = obj.configmap();
     let deployment = obj.deployment(&configmap);
     let service = obj.service();
-
-    let deployment_api = Api::<Deployment>::namespaced(ctx.client.clone(), obj.namespace());
-    let service_api =
-        Api::<k8s_openapi::api::core::v1::Service>::namespaced(ctx.client.clone(), obj.namespace());
-    let configmap_api = Api::<k8s_openapi::api::core::v1::ConfigMap>::namespaced(
-        ctx.client.clone(),
-        obj.namespace(),
-    );
 
     let deployment_result = deployment_api
         .patch(
---
+++ src/user/secret.rs
@@ -0,0 +1,28 @@
+use crate::v1::MqttUser;
+use k8s_openapi::api::core::v1::Secret;
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta;
+use kube::Resource;
+use std::collections::BTreeMap;
+
+impl MqttUser {
+    #[must_use]
+    pub fn secret(&self) -> Secret {
+        let name = format!("{}-{}", self.spec.broker_ref.name, self.spec.username);
+
+        let password = crate::util::generate_password();
+        let mut data = BTreeMap::new();
+        data.insert("password".to_string(), password);
+        data.insert("username".to_string(), self.spec.username.clone());
+
+        Secret {
+            metadata: ObjectMeta {
+                name: Some(name),
+                namespace: self.metadata.namespace.clone(),
+                owner_references: Some(vec![self.controller_owner_ref(&()).unwrap()]),
+                ..Default::default()
+            },
+            string_data: Some(data),
+            ..Default::default()
+        }
+    }
+}
